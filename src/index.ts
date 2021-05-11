4d3ca9ed6bfe9af658464b862b995ba94ad12054
Greg Linscheid <linscheid.greg@gmail.com>
2021-05-11 00:45:16 -0700

    :shirt: Final notes.

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,52 @@ interface Callback {
 
 type Renderer = PIXI.Renderer | PIXI.AbstractRenderer;
 
+// TODO:
+// First graduate this to having helper functions, to update value of bird and sprites (create class?)
+// Eventually upgrade to redux.
+interface StageState {
+  bird?: PIXI.AnimatedSprite;
+}
+
+/**
+ * Constants.
+ */
+
 declare const VERSION: string;
 
-const gameWidth = 800;
-const gameHeight = 600;
+const gameWidth = () => window.innerWidth;
+const gameHeight = () => window.innerHeight;
+
+/**
+ * Main functions.
+ */
 
 startGame();
 
 function startGame() {
+  const stageState: StageState = {};
+
+  // Create a PixiJS application.
   const app = createApp();
 
-  const { stage, view, renderer } = app;
+  const { renderer, stage, ticker, view } = app;
 
-  const resize = async (): Promise<void> => onResize(renderer);
+  // Hook for browser window resizes.
+  const resize = async (): Promise<void> => onResize(renderer, stageState);
   resize();
 
-  const onload = async (): Promise<void> => onLoadGame(stage, view);
+  // Hook for initial loading of assets.
+  const onload = async (): Promise<void> => onLoad(stage, view, stageState);
 
+  // Attach hooks to window.
   setupWindowHooks(onload, resize);
+
+  // Callback for game loop.
+  const onGameLoop = () => gameLoop(renderer, stage, stageState);
+
+  // Attach and start game loop.
+  ticker.add(onGameLoop);
+  ticker.start();
 
   console.log(`Welcome to Catfish in Space v${VERSION}`);
 }
@@ -39,33 +67,53 @@ function setupWindowHooks(onload: Callback, resize: Callback) {
 }
 
 /**
+ * Primary game loop function, which is responsible for
+ * making changes to the current stage.
+ */
+function gameLoop(renderer: Renderer, stage: PIXI.Container, stageState: StageState) {
+  if (stageState.bird) {
+    stageState.bird.position.set(gameWidth() / 2, gameHeight() / 2);
+
+    stageState.bird.rotation += 0.1;
+  }
+
+  renderer.render(stage);
+}
+
+/**
  * Helpers.
  */
 
 function createApp() {
   return new PIXI.Application({
     backgroundColor: 0xd3d3d3,
-    width: gameWidth,
-    height: gameHeight,
+    width: gameWidth(),
+    height: gameHeight(),
+    resolution: window.devicePixelRatio,
+    autoDensity: true,
   });
 }
 
-async function onResize(renderer: Renderer) {
-  renderer.resize(gameWidth, gameHeight);
+async function onResize(renderer: Renderer, stageState: StageState) {
+  if (stageState.bird) {
+    stageState.bird.position.set(gameWidth() / 2, gameHeight() / 2);
+  }
 
-  // Currently a noop, but could redefine to scale to browser width/height.
-  //app.renderer.resize(window.innerWidth, window.innerHeight);
-  //app.stage.scale.x = window.innerWidth / gameWidth;
-  //app.stage.scale.y = window.innerHeight / gameHeight;
+  // TODO: Gracefully handle window resizes.
+  // Keep list of all sprites
+  // on window resize, compute distance needed to move to keep player centered (ducky)
+  // move all sprites by that much
+
+  renderer.resize(gameWidth(), gameHeight());
 }
 
-async function onLoadGame(stage: PIXI.Container, view: HTMLCanvasElement) {
+async function onLoad(stage: PIXI.Container, view: HTMLCanvasElement, stageState: StageState) {
   // Do not append the game view to the DOM, until the assets are loaded.
   await loadGameAssets();
 
   document.body.appendChild(view);
 
-  setupStage(stage);
+  setupStage(stage, stageState);
 }
 
 /**
@@ -92,12 +140,14 @@ async function loadGameAssets(): Promise<void> {
 /**
  * Setup the stage of the game, by adding initial elements.
  */
-function setupStage(stage: PIXI.Container): void {
+function setupStage(stage: PIXI.Container, stageState: StageState) {
   const birdFromSprite = getBird();
   birdFromSprite.anchor.set(0.5, 0.5);
-  birdFromSprite.position.set(gameWidth / 2, gameHeight / 2);
+  birdFromSprite.position.set(gameWidth() / 2, gameHeight() / 2);
 
   stage.addChild(birdFromSprite);
+
+  stageState.bird = birdFromSprite;
 }
 
 /**
