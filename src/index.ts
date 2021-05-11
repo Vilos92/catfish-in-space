64182405e67ebb2def239346740455241c352048
Greg Linscheid <linscheid.greg@gmail.com>
2021-05-10 23:38:24 -0700

    :bulb: Make things more functional throughout.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import "./style.css";
  * Types.
  */
 
-interface CallbackThreeParam<T1, T2, T3, T4 = void> {
-    (param1: T1, param2: T2, param3: T3): T4;
+interface Callback {
+  (): void;
 }
 
 type Renderer = PIXI.Renderer | PIXI.AbstractRenderer;
@@ -19,24 +19,23 @@ const gameHeight = 600;
 startGame();
 
 function startGame() {
-    const app = createApp();
+  const app = createApp();
 
-    const { stage, view, renderer } = app;
+  const { stage, view, renderer } = app;
 
-    setupWindowHooks(stage, view, renderer, onload);
+  const resize = async (): Promise<void> => onResize(renderer);
+  resize();
 
-    console.log(`Welcome to Catfish in Space v${VERSION}`);
+  const onload = async (): Promise<void> => onLoadGame(stage, view);
+
+  setupWindowHooks(onload, resize);
+
+  console.log(`Welcome to Catfish in Space v${VERSION}`);
 }
 
-function setupWindowHooks(
-    stage: PIXI.Container,
-    view: HTMLCanvasElement,
-    renderer: Renderer,
-    onload: CallbackThreeParam<PIXI.Container, HTMLCanvasElement, Renderer>
-) {
-    window.onload = async (): Promise<void> => {
-        onload(stage, view, renderer);
-    };
+function setupWindowHooks(onload: Callback, resize: Callback) {
+  window.onload = onload;
+  window.addEventListener("resize", resize);
 }
 
 /**
@@ -44,70 +43,77 @@ function setupWindowHooks(
  */
 
 function createApp() {
-    return new PIXI.Application({
-        backgroundColor: 0xd3d3d3,
-        width: gameWidth,
-        height: gameHeight,
-    });
+  return new PIXI.Application({
+    backgroundColor: 0xd3d3d3,
+    width: gameWidth,
+    height: gameHeight,
+  });
 }
 
-async function onload(stage: PIXI.Container, view: HTMLCanvasElement, renderer: Renderer) {
-    await loadGameAssets();
+async function onResize(renderer: Renderer) {
+  renderer.resize(gameWidth, gameHeight);
 
-    document.body.appendChild(view);
-
-    resizeCanvas(renderer);
-
-    const birdFromSprite = getBird();
-    birdFromSprite.anchor.set(0.5, 0.5);
-    birdFromSprite.position.set(gameWidth / 2, gameHeight / 2);
-
-    stage.addChild(birdFromSprite);
+  // Currently a noop, but could redefine to scale to browser width/height.
+  //app.renderer.resize(window.innerWidth, window.innerHeight);
+  //app.stage.scale.x = window.innerWidth / gameWidth;
+  //app.stage.scale.y = window.innerHeight / gameHeight;
 }
 
+async function onLoadGame(stage: PIXI.Container, view: HTMLCanvasElement) {
+  // Do not append the game view to the DOM, until the assets are loaded.
+  await loadGameAssets();
+
+  document.body.appendChild(view);
+
+  setupStage(stage);
+}
+
+/**
+ * Load game assets as a side-effect (no return value).
+ * Currently these assets are just a sprite sheet.
+ */
 async function loadGameAssets(): Promise<void> {
-    return new Promise((res, rej) => {
-        const loader = PIXI.Loader.shared;
-        loader.add("rabbit", "./assets/simpleSpriteSheet.json");
+  return new Promise((res, rej) => {
+    const loader = PIXI.Loader.shared;
+    loader.add("rabbit", "./assets/simpleSpriteSheet.json");
 
-        loader.onComplete.once(() => {
-            res();
-        });
-
-        loader.onError.once(() => {
-            rej();
-        });
-
-        loader.load();
+    loader.onComplete.once(() => {
+      res();
     });
+
+    loader.onError.once(() => {
+      rej();
+    });
+
+    loader.load();
+  });
 }
 
-function resizeCanvas(renderer: Renderer): void {
-    const resize = () => {
-        renderer.resize(gameWidth, gameHeight);
+/**
+ * Setup the stage of the game, by adding initial elements.
+ */
+function setupStage(stage: PIXI.Container): void {
+  const birdFromSprite = getBird();
+  birdFromSprite.anchor.set(0.5, 0.5);
+  birdFromSprite.position.set(gameWidth / 2, gameHeight / 2);
 
-        // Currently a noop, but could redefine to scale to browser width/height.
-        //app.renderer.resize(window.innerWidth, window.innerHeight);
-        //app.stage.scale.x = window.innerWidth / gameWidth;
-        //app.stage.scale.y = window.innerHeight / gameHeight;
-    };
-
-    resize();
-
-    window.addEventListener("resize", resize);
+  stage.addChild(birdFromSprite);
 }
 
+/**
+ * Test method which loads a bird from the sprite sheet, and returns the AnimatedSprite.
+ */
 function getBird(): PIXI.AnimatedSprite {
-    const bird = new PIXI.AnimatedSprite([
-        PIXI.Texture.from("birdUp.png"),
-        PIXI.Texture.from("birdMiddle.png"),
-        PIXI.Texture.from("birdDown.png"),
-    ]);
+  const bird = new PIXI.AnimatedSprite([
+    PIXI.Texture.from("birdUp.png"),
+    PIXI.Texture.from("birdMiddle.png"),
+    PIXI.Texture.from("birdDown.png"),
+  ]);
 
-    bird.loop = true;
-    bird.animationSpeed = 0.1;
-    bird.play();
-    bird.scale.set(3);
+  bird.loop = true;
+  bird.animationSpeed = 0.1;
+  bird.play();
+  bird.scale.set(3);
 
-    return bird;
+  return bird;
 }
